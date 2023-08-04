b43e3bc9ffe64784b00c78e73d1926484ea098a8
mdomore <michael.moreliere@gmail.com>
2023-08-04 17:30:12 +0200

    Adding link for the recipe Change Meal list to add name and url value

--- index.js
+++ index.js
@@ -1,62 +1,233 @@
 // Array to store Meals
 var meals = [
-	"Viande - légumes plancha",
-	"Tomates farcies",
-	"Tomates - mozza",
-	"Tartiflette",
-	"Steak haché - frites",
-	"Soupe - croûtons",
-	"Salade lardons",
-	"Salade epinard - poulet - parmesan - tomates confites",
-	"Salade caesar",
-	"Riz chinois",
-	"Risotto speck - petits pois",
-	"Risotto chorizo - petits pois",
-	"Raviolis frais - sauce tomate",
-	"Ratatouille",
-	"Raclette",
-	"Quiche lorraine - quiche saumon",
-	"Quiche carottes - courgettes",
-	"Pâtes à la viande",
-	"Pâtes à la carbonara",
-	"Pâtes sauce",
-	"Pâtes roquette",
-	"Pâtes noisettes - salade - parmesan",
-	"Pâtes au saumon",
-	"Pâtes Udon - poulet",
-	"Purée - saucisses",
-	"Poulet papillotte - riz",
-	"Potimarron - carottes - marrons - four",
-	"Pommes de terre - Carottes - Courgettes - Saucisse",
-	"Pomme de terre - carottes rôties",
-	"Poissons - riz",
-	"Pizza",
-	"Omelette - salade",
-	"Oeufs à la coque",
-	"Melon - jambon",
-	"Lentilles - saucisses",
-	"Lasagnes maison",
-	"Jardinière de légumes",
-	"Hâchis parmentier maison",
-	"Haricots verts - lardons - pdt",
-	"Grâtin pommes de terre - carottes",
-	"Grâtin macaronis - jambon",
-	"Gougères - salade",
-	"Galettes",
-	"Escalope à la crème - riz",
-	"Croque-mozza",
-	"Croque-monsieur",
-	"Couscous",
-	"Cordons bleus - petits pois",
-	"Chili con carne",
-	"Buritos",
-	"Boulettes viande - semoule",
-	"Tempura de légumes",
-	"Poulet Teriyaki - riz, concombres",
-	"Poisson - Haricots verts",
-	"Poulet aux pruneaux - riz sarfané - oignons",
-	"Curry de chou-fleur rôti et lentilles corail", // https://chefsimon.com/gourmets/radisrose/recettes/curry-de-chou-fleur-roti-et-lentilles-corail
-	"Gaufres salées à la Patate douce", // https://cuisine-addict.com/gaufres-salees-a-la-patate-douce/
+	{
+		"name": "Viande - légumes plancha",
+		"link": ""
+	},
+	{
+		"name": "Tomates farcies",
+		"link": ""
+	},
+	{
+		"name": "Tomates - mozza",
+		"link": ""
+	},
+	{
+		"name": "Tartiflette",
+		"link": ""
+	},
+	{
+		"name": "Steak haché - frites",
+		"link": ""
+	},
+	{
+		"name": "Soupe - croûtons",
+		"link": ""
+	},
+	{
+		"name": "Salade lardons",
+		"link": ""
+	},
+	{
+		"name": "Salade epinard - poulet - parmesan - tomates confites",
+		"link": ""
+	},
+	{
+		"name": "Salade caesar",
+		"link": ""
+	},
+	{
+		"name": "Riz chinois",
+		"link": ""
+	},
+	{
+		"name": "Risotto speck - petits pois",
+		"link": ""
+	},
+	{
+		"name": "Risotto chorizo - petits pois",
+		"link": ""
+	},
+	{
+		"name": "Raviolis frais - sauce tomate",
+		"link": ""
+	},
+	{
+		"name": "Ratatouille",
+		"link": ""
+	},
+	{
+		"name": "Raclette",
+		"link": ""
+	},
+	{
+		"name": "Quiche lorraine - quiche saumon",
+		"link": ""
+	},
+	{
+		"name": "Quiche carottes - courgettes",
+		"link": ""
+	},
+	{
+		"name": "Pâtes à la viande",
+		"link": ""
+	},
+	{
+		"name": "Pâtes à la carbonara",
+		"link": ""
+	},
+	{
+		"name": "Pâtes sauce",
+		"link": ""
+	},
+	{
+		"name": "Pâtes roquette",
+		"link": ""
+	},
+	{
+		"name": "Pâtes noisettes - salade - parmesan",
+		"link": ""
+	},
+	{
+		"name": "Pâtes au saumon",
+		"link": ""
+	},
+	{
+		"name": "Pâtes Udon - poulet",
+		"link": ""
+	},
+	{
+		"name": "Purée - saucisses",
+		"link": ""
+	},
+	{
+		"name": "Poulet papillotte - riz",
+		"link": ""
+	},
+	{
+		"name": "Potimarron - carottes - marrons - four",
+		"link": ""
+	},
+	{
+		"name": "Pommes de terre - Carottes - Courgettes - Saucisse",
+		"link": ""
+	},
+	{
+		"name": "Pomme de terre - carottes rôties",
+		"link": ""
+	},
+	{
+		"name": "Poissons - riz",
+		"link": ""
+	},
+	{
+		"name": "Pizza",
+		"link": ""
+	},
+	{
+		"name": "Omelette - salade",
+		"link": ""
+	},
+	{
+		"name": "Oeufs à la coque",
+		"link": ""
+	},
+	{
+		"name": "Melon - jambon",
+		"link": ""
+	},
+	{
+		"name": "Lentilles - saucisses",
+		"link": ""
+	},
+	{
+		"name": "Lasagnes maison",
+		"link": ""
+	},
+	{
+		"name": "Jardinière de légumes",
+		"link": ""
+	},
+	{
+		"name": "Hâchis parmentier maison",
+		"link": ""
+	},
+	{
+		"name": "Haricots verts - lardons - pdt",
+		"link": ""
+	},
+	{
+		"name": "Grâtin pommes de terre - carottes",
+		"link": ""
+	},
+	{
+		"name": "Grâtin macaronis - jambon",
+		"link": ""
+	},
+	{
+		"name": "Gougères - salade",
+		"link": ""
+	},
+	{
+		"name": "Galettes",
+		"link": ""
+	},
+	{
+		"name": "Escalope à la crème - riz",
+		"link": ""
+	},
+	{
+		"name": "Croque-mozza",
+		"link": ""
+	},
+	{
+		"name": "Croque-monsieur",
+		"link": ""
+	},
+	{
+		"name": "Couscous",
+		"link": ""
+	},
+	{
+		"name": "Cordons bleus - petits pois",
+		"link": ""
+	},
+	{
+		"name": "Chili con carne",
+		"link": ""
+	},
+	{
+		"name": "Buritos",
+		"link": ""
+	},
+	{
+		"name": "Boulettes viande - semoule",
+		"link": ""
+	},
+	{
+		"name": "Tempura de légumes",
+		"link": ""
+	},
+	{
+		"name": "Poulet Teriyaki - riz, concombres",
+		"link": ""
+	},
+	{
+		"name": "Poisson - Haricots verts",
+		"link": ""
+	},
+	{
+		"name": "Poulet aux pruneaux - riz sarfané - oignons",
+		"link": ""
+	},
+	{
+		"name": "Curry de chou-fleur rôti et lentilles corail",
+		"link": "https://chefsimon.com/gourmets/radisrose/recettes/curry-de-chou-fleur-roti-et-lentilles-corail"
+	},
+	{
+		"name": "Gaufres salées à la Patate douce",
+		"link": "https://cuisine-addict.com/gaufres-salees-a-la-patate-douce/"
+	},
 ];
 
 // Array to store tags
@@ -92,9 +263,11 @@ var days = [
 // Loop to generate a random meal for each meal in the week
 for (day of days) {
   var randomNumber = Math.floor(Math.random() * meals.length);
-  $("." + day +" .meal.lunch .meal-title").text(meals[randomNumber]);
+  $("." + day +" .meal.lunch .meal-title").text(meals[randomNumber].name);
+	$("." + day +" .meal.lunch .href-link").attr("href", meals[randomNumber].link);
   var randomNumber = Math.floor(Math.random() * meals.length);
-  $("." + day +" .meal.diner .meal-title").text(meals[randomNumber]);
+  $("." + day +" .meal.diner .meal-title").text(meals[randomNumber].name);
+	$("." + day +" .meal.diner .href-link").attr("href", meals[randomNumber].link);
 }
 
 // Refresh button implementation
@@ -102,7 +275,8 @@ $(".option-refresh").click(function(e) {
   // console.log($(this).closest(".meal").children(".meal-title"));
   e.preventDefault();
   var randomNumber = Math.floor(Math.random() * meals.length);
-  $(this).closest(".meal").children(".meal-title").text(meals[randomNumber]);
+  $(this).closest(".meal").children(".meal-title").text(meals[randomNumber].name);
+	$(this).closest(".meal").children(".href-link").attr("href", meals[randomNumber].link);
   return false;
 })
 
